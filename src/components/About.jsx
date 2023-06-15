0dc923f0134315f3c7ab41eb77f05576e198d56e
jorge chirinos <jorgeanciel29@hotmail.com>
2023-06-14 20:25:17 -0500

    80%

---
+++ src/components/About.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import Logo1 from "../assets/JavaScript-logo.png";
+import Logo2 from "../assets/React-icon.svg.png";
+import Logo3 from "../assets/nodeJS.png";
+import Logo4 from "../assets/taildwind.png";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+
+const About = () => {
+  return (
+    <section className="section" id="about">
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 bg-about bg-contain bg-no-repeat h-[540px] mix-blend-lighten bg-top"
+          ></motion.div>
+          <motion.div
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
+            <h2 className="h2 text-gradient">About me</h2>
+            <h3 className="h3 mb-4">
+              I'm a Developer with over 1 year of experience
+            </h3>
+            <p className="mb-6">
+              Soy una persona proactiva, organizada y responsable, con buenas
+              relaciones interpersonales. Me destaco por el buen trabajo en
+              equipo y la rápida toma de decisiones. Busco un puesto de trabajo
+              desafiante para continuar aprendiendo.
+            </p>
+            <div className="flex gap-x-6 lg:gap-x-8 mb-12 flex-wrap">
+              <div>
+                <div className="text-[30px]">JavaScript</div>
+                <div className="flex justify-center">
+                  <img className="w-[100px]" src={Logo1} alt="" />
+                </div>
+              </div>
+              <div>
+                <div className="text-[30px]">React</div>
+                <div className="flex justify-center">
+                  <img className="w-[60px] pt-2" src={Logo2} alt="" />
+                </div>
+              </div>
+              <div>
+                <div className="text-[30px]">NodeJs</div>
+                <div className="flex justify-center">
+                  <img className="w-[55px] pt-1" src={Logo3} alt="" />
+                </div>
+              </div>
+              <div>
+                <div className="text-[30px]">Taildwind</div>
+                <div className="flex justify-center">
+                  <img className="w-[70px]" src={Logo4} alt="" />
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-x-8 items-center mx-auto">
+              <button className="btn p-3">Contact me</button>
+              <a
+                href="https://www.linkedin.com/in/jorgechirinosalferez"
+                className="text-gradient btn-link"
+                target="__blank"
+              >
+                My Linkedin
+              </a>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Ikaro from "../assets/ikaro.png";
+
+const Header = () => {
+  return (
+    <header className=" py-8">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <a href="#">
+            <img className="w-24" src={Ikaro} alt="" />
+          </a>
+          <button className="btn btn-sm">Work with me</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
